64dff6b157ec9be843f00e6e50c5a545e7ab524f
KozaPawel <p.koza99@o2.pl>
2023-12-03 22:24:43 +0100

    Highlight menu item when using arrows to navigate

--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -44,9 +44,23 @@
                                 class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
                             >
                                 <div class="px-1 py-1">
+                                    <!-- <button
+                                                :class="[
+                                                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                                ]"
+                                            >
+                                                <UserIcon
+                                                    :active="active"
+                                                    class="mr-2 h-5 w-5 text-violet-400"
+                                                    aria-hidden="true"
+                                                />
+                                                Edit
+                                            </button> -->
+
                                     <Link :href="route('user.listing.index')"> 
-                                        <MenuItem>
-                                            <button class="menu-item">
+                                        <MenuItem v-slot="{ active }">
+                                            <button :class="[active ? 'bg-light-hover-gray' : '', 'menu-item' ]">
                                                 <UserIcon
                                                     class="menu-icon"
                                                     aria-hidden="true"
@@ -57,8 +71,8 @@
                                     </Link>
 
                                     <Link :href="route('user.listing.create')"> 
-                                        <MenuItem>
-                                            <button class="menu-item">
+                                        <MenuItem v-slot="{ active }">
+                                            <button :class="[active ? 'bg-light-hover-gray' : '', 'menu-item' ]">
                                                 <PlusIcon
                                                     class="menu-icon"
                                                     aria-hidden="true"
@@ -77,9 +91,8 @@
                                         class="w-full"
                                     > 
                                         <MenuItem v-slot="{ active }">
-                                            <button class="menu-item hover:bg-[#C1C4C6]">
+                                            <button :class="[active ? 'bg-[#C1C4C6]' : '', 'menu-item hover:bg-[#C1C4C6]' ]">
                                                 <ArrowRightOnRectangleIcon
-                                                    :active="active"
                                                     class="menu-icon"
                                                     aria-hidden="true"
                                                 />
