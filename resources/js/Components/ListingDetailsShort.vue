d35d1da08fb52465908762026d4f970a1138c45e
KozaPawel <p.koza99@o2.pl>
2024-01-14 22:43:26 +0100

    Change offers style

--- resources/js/Components/ListingDetailsShort.vue
+++ resources/js/Components/ListingDetailsShort.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="pl-4 p-3">
-        <p class="font-medium text-xl mb-2">{{ listing.name }}</p>
+        <p class="font-medium text-xl mb-2 break-all">{{ listing.name }}</p>
         <p class="text-sm text-gray-600 break-words">
             <TownInfo :town="listing.town" />
         </p>
--- resources/js/Components/MakeOffer.vue
+++ resources/js/Components/MakeOffer.vue
@@ -1,14 +1,23 @@
 <template>
     <Box>
-        <template #header>Przedmioty które możesz wymienić</template>
+        <template #header>Twoje przedmioty</template>
 
         <form @submit.prevent="makeOffer">
-            <div v-for="listing in userListings" :key="listing.id" class="flex items-center gap-2">
-                <input :id="listing.id" :key="listing.id" v-model="form.offerId" type="radio" :value="listing.id" />
-                <label :key="listing.id" :for="listing.id">{{ listing.name }}</label>
+            <div class="grid grid-cols-12 gap-2 h-80 mt-2 overflow-y-auto p-1">
+                <div
+                    v-for="listing in userListings" :key="listing.id" class="col-span-6 xl:col-span-4 rounded-md" 
+                    :class="{'transition duration-200 ease-in outline-none border-light-focus ring-2 ring-light-focus': form.offerId === listing.id}"
+                >
+                    <input :id="listing.id" :key="listing.id" v-model="form.offerId" type="radio" :value="listing.id" class="hidden" />
+                    <label :key="listing.id" :for="listing.id" class="hover:cursor-pointer">
+                        <img v-if="listing.images.length" :src="listing.images[0].source" class="aspect-square h-32 w-full object-cover rounded-t-md" />
+                        <NoPhotos v-else class="aspect-square h-32 w-full object-cover rounded-t-md" />
+                        <ListingDetailsShort :listing="listing" />    
+                    </label>
+                </div>
             </div>
 
-            <button type="submit" class="btn-outline w-full mt-2 text-sm">Złóż ofertę</button>
+            <button type="submit" class="btn-primary w-full mt-2 text-sm col-span-12">Złóż ofertę</button>
             <ErrorMessage class="mt-2" :error="form.errors.offerId" />
         </form>
     </Box>
@@ -18,6 +27,8 @@
 import { useForm } from '@inertiajs/vue3'
 import Box from '@/Components/UI/Box.vue'
 import ErrorMessage from './ErrorMessage.vue'
+import ListingDetailsShort from './ListingDetailsShort.vue'
+import NoPhotos from './NoPhotos.vue'
 
 const form = useForm({
     offerId: props.offerId,
