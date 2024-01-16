868b9444fcb7a4c7859b56a739c871abe4a492c9
KozaPawel <p.koza99@o2.pl>
2024-01-16 16:39:31 +0100

    Add accordion to category filter

--- resources/js/Components/ListingsFilter.vue
+++ resources/js/Components/ListingsFilter.vue
@@ -3,20 +3,34 @@
         <form @submit.prevent="filter">
             <div class="box mb-4 flex flex-wrap flex-col gap-2 items-start w-full ">
                 <h2 class="text-2xl font-medium">Filtry</h2>
-                <h2 class="text-xl">Kategoria</h2>
                 
-                <div v-for="category in categories" :key="category.id" class="flex flex-nowrap items-center gap-2">
-                    <input
-                        :id="category.id" 
-                        v-model="filterForm.categories"
-                        :value="category.id"
-                        type="checkbox" 
-                        class="input-checkbox"
-                    />
-                    <label :for="category.id" class="hover:cursor-pointer hover:underline break-all">{{ category.name }}</label>
+                <div class="w-full">
+                    <Disclosure v-slot="{ open }">
+                        <DisclosureButton
+                            class="btn-accent flex items-center w-full justify-between rounded-lg"
+                        >
+                            <span class="text-xl">Kategoria</span>
+                            <ChevronDownIcon
+                                :class="open ? 'rotate-180 transform' : ''"
+                                class="h-5 w-5 transition duration-100 ease-in"
+                            />
+                        </DisclosureButton>
+                        <DisclosurePanel class="pl-2 pb-2 pt-4">
+                            <div v-for="category in categories" :key="category.id" class="flex flex-nowrap items-center gap-2">
+                                <input
+                                    :id="category.id" 
+                                    v-model="filterForm.categories"
+                                    :value="category.id"
+                                    type="checkbox" 
+                                    class="input-checkbox"
+                                />
+                                <label :for="category.id" class="hover:cursor-pointer hover:underline break-words">{{ category.name }}</label>
+                            </div>
+                        </DisclosurePanel>
+                    </Disclosure>
                 </div>
                 
-                <div class="flex flex-col items-center mt-4">
+                <div class="flex flex-col items-center">
                     <button type="submit" class="w-fit btn-accent">Zastosuj filtry</button>
                     <button type="reset" class="w-fit hover:underline text-sm mt-2" @click="reset">Wyczyść filtry</button>
                 </div>
@@ -28,6 +42,8 @@
 <script setup>
 import { reactive, computed } from 'vue'
 import { router, usePage } from '@inertiajs/vue3'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 
 const page = usePage()
 
